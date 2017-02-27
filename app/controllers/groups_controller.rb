43065f0c328b3a240ca4003a4a456481f5da0f4d
whispeed <whispeed@163.com>
2017-02-27 22:01:37 +0800

    validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,6 +3,10 @@ class GroupsController < ApplicationController
     @groups = Group.all
   end
 
+  def new
+    @group = Group.new
+  end
+
   def show
     @group = Group.find(params[:id])
   end
@@ -11,30 +15,28 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
-  def new
-    @group = Group.new
-  end
-
   def create
     @group = Group.new(group_params)
-    @group.save
-
-    redirect_to groups_path
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
   end
 
   def update
     @group = Group.find(params[:id])
-
-    @group.update(group_params)
-
-    redirect_to group_path, notice: "Update Seccess"
+    if @group.update(group_params)
+      redirect_to groups_path, notice: 'Update Success'
+    else
+      render :edit
+    end
   end
 
   def destroy
     @group = Group.find(params[:id])
     @group.destroy
-    flash[:alert] = "Group deleted"
-    redirect_to groups_path
+    redirect_to groups_path, alert: 'Group deleted'
   end
 
   private
